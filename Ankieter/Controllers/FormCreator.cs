f8485e020dc242495abcb43f3cfcc57307310d63
Maciej Kolodziej <mkolodziej@pgs-soft.com>
2017-10-11 14:12:59 +0200

    basic form creator structure

---
+++ Ankieter/Controllers/FormCreator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ankieter.Controllers
+{
+    public class FormCreator : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Ankieter/Data/ApplicationDbContext.cs
+++ Ankieter/Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Ankieter.Models;
+using Ankieter.Models.Forms;
 
 namespace Ankieter.Data
 {
@@ -22,5 +23,7 @@ namespace Ankieter.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<Ankieter.Models.Forms.CreatedForm> CreatedForm { get; set; }
     }
 }
