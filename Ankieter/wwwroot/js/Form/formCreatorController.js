b232230a5def1aa137ede6a34fa677c45f00cb30
Maciej Kolodziej <mkolodziej@pgs-soft.com>
2017-10-11 15:22:52 +0200

    added angularjs

---
+++ Ankieter/wwwroot/js/Form/formCreatorController.js
@@ -0,0 +1,27 @@
+﻿var myApp = angular.module('myApp', []);
+
+myApp.controller('formCreatorController', ['$scope', function ($scope) {
+    $scope.lastInputId = 0;
+    $scope.inputs = [];
+
+    $scope.generateUIForInput = function (input) {
+        return "<div></div>";
+    }
+
+    $scope.renderNewInputOnUI = function (input) {
+        $("#inputList").innerHTML += generateUIForInput(input);
+    }
+    
+    $scope.inputs.push(
+    {
+        id: $scope.lastInputId++,
+        type: "not selected",
+        isRequired: true
+    });
+    $scope.inputs.push(
+    {
+        id: $scope.lastInputId++,
+        type: "not selected",
+        isRequired: true
+    });
+}]);
