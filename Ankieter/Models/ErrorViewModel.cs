20a9c7dd8cd502b5119c16e54a3621f14b8c6395
Maciej Kolodziej <mkolodziej@pgs-soft.com>
2017-10-11 12:12:50 +0200

    initial

---
+++ Ankieter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ankieter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
